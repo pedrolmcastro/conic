81080756fd021a228f182f76df1c57b067062cd4
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-02-17 05:14:13 -0400

    Defining __repr__ in Equation and renaming variables
    - Adding a __repr__ definition to Equation.
    - Renaming the coefficients dictionary in Equation from 'coeff' to 'coeffs'.
    - Renaming the mapping dictionary in __str__ of Equation from 'var' to 'coeffToVar'.
    - Removing the abs() call in __str__ of Equation.
    - Removing extra blank lines in equation.py.

--- equation.py
+++ equation.py
@@ -5,18 +5,17 @@ class Equation:
     A conic equation general form representation.
     
     Attributes:
-        coeff (dict of float): all equation coefficients.
+        coeffs (dict of float): all equation coefficients.
     """
 
     def __init__(self, a=1, b=1, c=1, d=0, e=0, f=0):
-        self.coeff = dict()
-        self.coeff['a'] = real.parse(a)
-        self.coeff['b'] = real.parse(b)
-        self.coeff['c'] = real.parse(c)
-        self.coeff['d'] = real.parse(d)
-        self.coeff['e'] = real.parse(e)
-        self.coeff['f'] = real.parse(f)
-
+        self.coeffs = dict()
+        self.coeffs['a'] = real.parse(a)
+        self.coeffs['b'] = real.parse(b)
+        self.coeffs['c'] = real.parse(c)
+        self.coeffs['d'] = real.parse(d)
+        self.coeffs['e'] = real.parse(e)
+        self.coeffs['f'] = real.parse(f)
 
     @classmethod
     def get(cls):
@@ -27,23 +26,27 @@ class Equation:
         e = real.get('e: ')
         f = real.get('f: ')
         return cls(a, b, c, d, e, f)
-    
+
+    def __repr__(self):
+        return (f'{self.__class__.__module__}.{self.__class__.__qualname__}' + 
+                f'({self.coeffs["a"]}, {self.coeffs["b"]}, {self.coeffs["c"]}, {self.coeffs["d"]}, {self.coeffs["e"]}, {self.coeffs["f"]})')
 
     def __str__(self):
-        var = { 'a' : 'x²',
-                'b' : 'xy',
-                'c' : 'y²',
-                'd' : 'x',
-                'e' : 'y',
-                'f' : '' }
-        
+        coeffToVar = {
+            'a': 'x²',
+            'b': 'xy',
+            'c': 'y²',
+            'd': 'x',
+            'e': 'y',
+            'f': '',
+        }
+
         string = ''
-        for coeff, val in self.coeff.items():
-            if coeff in var.keys():
+        for coeff, val in self.coeffs.items():
+            if coeff in coeffToVar.keys():
                 if val < 0:
-                    string += f'- {abs(val)}{var[coeff]} '
+                    string += f'- {-val}{coeffToVar[coeff]} '
                 elif val > 0:
-                    string += f'+ {abs(val)}{var[coeff]} '
+                    string += f'+ {val}{coeffToVar[coeff]} '
                 # coeff with val == 0 is not printed
-        
         return string
--- main.py
+++ main.py
@@ -2,9 +2,4 @@ from equation import Equation
 
 test = Equation.get()
 print(test)
-
-test.coeff['j'] = 32
-for i in test.coeff.keys():
-    print(i, end=' ')
-print()
-print(test)
+print(repr(test))
