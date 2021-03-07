86b0558b813b351c7074562c45d5dcd277104689
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-03-06 23:07:34 -0400

    Rebuilding _findRotationAngle() to use math.atan2()
    - Deleting _isDependentSystem() method from Conic.
    - Reorganizing _findCenter() comments.
    - Rebuilding _findRotationAngle() to use math.atan2() now.
    - Making _rotate() safe from ZeroDivision and reorganizing its comments.
    - Reorganizing _findName() comments.

--- conic.py
+++ conic.py
@@ -24,7 +24,7 @@ class _Point:
 
 class _Equation:
     """
-    A conic equation general form representation.
+    A conic general form equation representation.
     
     Attributes:
         a (float): coefficient of x².
@@ -126,18 +126,11 @@ class Conic:
         else:
             return False
 
-    def _isDependentSystem(self):
-        if self._eqt.a != 0 and math.isclose(self._eqt.e, (self._eqt.b*self._eqt.d) / (2*self._eqt.a)): #e = bd/2a
-            return True
-        elif self._eqt.c != 0 and math.isclose(self._eqt.d, (self._eqt.b*self._eqt.e) / (2*self._eqt.c)): #d = be/2c
-            return True
-        else:
-            return False
-
     def _findCenter(self):
         #ah + bk/2 + d/2 = 0
         #bh/2 + ck + e/2 = 0
-        if self.det != 0: #independent system
+        #independent system
+        if self.det != 0:
             if self._eqt.a == 0:
                 k = - self._eqt.d / self._eqt.b #k = -d/b
                 h = - (2*self._eqt.c*k + self._eqt.e) / self._eqt.b #h = -(2ck + e) / b
@@ -145,9 +138,13 @@ class Conic:
                 k = (self._eqt.b*self._eqt.d - 2*self._eqt.a*self._eqt.e) / (4*self.det) #k = (bd - 2ae) / (4*det)
                 h = - (self._eqt.b*k + self._eqt.d) / (2*self._eqt.a) #h = -(bk + d) / 2a
             self._ctr = _Point(h, k)
-        elif self._isDependentSystem():
+        #dependent system:
+        elif self._eqt.a != 0 and math.isclose(self._eqt.e, (self._eqt.b*self._eqt.d) / (2*self._eqt.a)): #e = bd/2a
             self._ctr = math.inf
-        else: #inconsistent system
+        elif self._eqt.c != 0 and math.isclose(self._eqt.d, (self._eqt.b*self._eqt.e) / (2*self._eqt.c)): #d = be/2c
+            self._ctr = math.inf
+        #inconsistent system
+        else:
             self._ctr = None
 
     def _translate(self):
@@ -167,43 +164,44 @@ class Conic:
             self._eqt.e = 0
 
     def _findRotationAngle(self):
-        #Trigonometric functions values of 2θ
-        doubleAngCotg = (self._eqt.a - self._eqt.c) / self._eqt.b #cotg(2θ) = (a-c)/b
-        doubleAngSin = 1 / math.sqrt(1 + doubleAngCotg**2) #sin(2θ) = 1/√1+cotg(2θ)²
-        doubleAngCos = doubleAngSin * doubleAngCotg #cos(2θ) = sin(2θ) * cotg(2θ)
-        #Trigonometric functions values of θ
-        angSin = math.sqrt((1 - doubleAngCos) / 2) #sin(θ) = √1-cos(2θ)
-        angCos = math.sqrt((1 + doubleAngCos) / 2) #cos(θ) = √1+cos(2θ)
-        self._ang = math.asin(angSin)
-        return angSin, angCos
+        if self._eqt.b == 0:
+            self._ang = 0
+        #cot(2θ) = (a-c)/b
+        elif self._eqt.a - self._eqt.c == 0:
+            self._ang = math.pi / 4 #2θ = pi/2 = acot(0)
+        else:
+            self._ang = math.atan2(self._eqt.b, self._eqt.a-self._eqt.c) / 2 #2θ = atan(b/(a-c))
 
     def _rotate(self):
-        #d' = dcos(θ) + esin(θ)
-        #e' = -dsin(θ) + ecos(θ)
-        angSin, angCos = self._findRotationAngle()
-        d = self._eqt.d
-        e = self._eqt.e
-        self._eqt.d = d*angCos + e*angSin #d' = dcos(θ) + esin(θ)
-        self._eqt.e = - d*angSin + e*angCos #e' = -dsin(θ) + ecos(θ)
-        #a' + c' = a + c
-        #a' - c' = b√1+((a-c)/b)²
-        a = self._eqt.a
-        c = self._eqt.c
-        self._eqt.a = (a + c + self._eqt.b*math.sqrt(1 + ((a-c)/self._eqt.b)**2)) / 2 #a' = a + c + b√1+((a-c)/b)²
-        self._eqt.c = a + c - self._eqt.a #c' = a + c - a'
-        self._eqt.b = 0
+        if self._eqt.b != 0:
+            self._findRotationAngle()
+            #d' = dcos(θ) + esin(θ)
+            #e' = -dsin(θ) + ecos(θ)
+            d = self._eqt.d
+            e = self._eqt.e
+            self._eqt.d = d*math.cos(self._ang) + e*math.sin(self._ang) #d' = dcos(θ) + esin(θ)
+            self._eqt.e = - d*math.sin(self._ang) + e*math.cos(self._ang) #e' = -dsin(θ) + ecos(θ)
+            #a' + c' = a + c
+            #a' - c' = b√1+((a-c)/b)²
+            a = self._eqt.a
+            c = self._eqt.c
+            self._eqt.a = (a + c + self._eqt.b*math.sqrt(1 + ((a-c)/self._eqt.b)**2)) / 2 #a' = (a + c + b√1+((a-c)/b)²) / 2
+            self._eqt.c = a + c - self._eqt.a #c' = a + c - a'
+            self._eqt.b = 0
 
     def _findName(self):
+        #nothing, point, circle, ellipse, hyperbola or intersecting lines
         if isinstance(self._ctr, _Point):
-            #nothing, point, circle, ellipse, hyperbola or intersecting lines
             if self._eqt.f == 0:
+                #ax² + cy² = 0
                 if self._eqt.a * self._eqt.c < 0:
                     self._name = 'intersecting lines'
-                else: #self._eqt.a * self._eqt.c > 0
+                else:
                     self._name = 'point'
             else:
                 A = - self._eqt.a / self._eqt.f
                 C = - self._eqt.c / self._eqt.f
+                #Ax² + Cy² = 1
                 if A * C < 0:
                     self._name = 'hyperbola'
                 elif A < 0 and C < 0:
@@ -213,16 +211,17 @@ class Conic:
                         self._name = 'circle'
                     else:
                         self._name = 'ellipse'
+        #nothing, parallel lines or coincident lines
         elif self._ctr == math.inf:
-            #nothing, parallel lines or coincident lines
-            if self._eqt.a*self._eqt.f < 0 or self._eqt.c*self._eqt.f < 0:
-                self._name = 'parallel lines'
-            elif self._eqt.f == 0:
+            #ax² + f = 0 or cy² + f = 0
+            if self._eqt.f == 0:
                 self._name = 'coincident lines'
-            else: #self._eqt.a*self._eqt.f > 0 or self._eqt.c*self._eqt.f > 0
+            elif self._eqt.a*self._eqt.f < 0 or self._eqt.c*self._eqt.f < 0:
+                self._name = 'parallel lines'
+            else:
                 self._name = 'nothing'
+        #nothing or parabola
         elif self._ctr == None:
-            #nothing or parabola
             if self._eqt.a != 0 and self._eqt.e != 0:
                 self._name = 'parabola'
             elif self._eqt.c != 0 and self._eqt.d != 0:
