d814b79ef1a00df00abc014efa8e1147790e13c7
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-04-21 15:46:12 -0400

    Renaming cnc to conic in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,17 +1,17 @@
 from conic import Conic
 
 
-cnc = Conic.frominput()
+conic = Conic.frominput()
 
-#cnc = Conic(1, 0, 1, 0, 0, 1) #nothing
-#cnc = Conic(1, 0, 1, 0, 0, 0) #point
-#cnc = Conic(1, 0, 1, -2, -2, -2) #circle
-#cnc = Conic(2, -1, 2, 0, 0, -30) #ellipse
-#cnc = Conic(1, 12, -4, 0, 0, -30) #hyperbola
-#cnc = Conic(1, 0, 0, 0, -1, 0) #parabola
-#cnc = Conic(0, 0, 1, 0, 0, -1) #parallel lines
-#cnc = Conic(1, 0, 0, 0, 0, 0) #coincident lines
-#cnc = Conic(1, 0, -1, 0, 0, 0) #intersecting lines
+# conic = Conic(1, 0, 1, 0, 0, 1)      # nothing
+# conic = Conic(1, 0, 1, 0, 0, 0)      # point
+# conic = Conic(1, 0, 1, -2, -2, -2)   # circle
+# conic = Conic(2, -1, 2, 0, 0, -30)   # ellipse
+# conic = Conic(1, 12, -4, 0, 0, -30)  # hyperbola
+# conic = Conic(1, 0, 0, 0, -1, 0)     # parabola
+# conic = Conic(0, 0, 1, 0, 0, -1)     # parallel lines
+# conic = Conic(1, 0, 0, 0, 0, 0)      # coincident lines
+# conic = Conic(1, 0, -1, 0, 0, 0)     # intersecting lines
 
-cnc.identify()
-print(cnc)
+conic.identify()
+print(conic)
