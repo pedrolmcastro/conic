4e861df699611b992b618f42dc2c105aff3da8d4
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-04-21 16:31:50 -0400

    Adding blank spaces in the beginning of comments

--- conic.py
+++ conic.py
@@ -64,9 +64,9 @@ class Conic:
         return any([getattr(self._equation, coefficient) != 0 for coefficient in ['a', 'b', 'c']])
 
     def _findCenter(self):
-        #ax + by/2 + d/2 = 0
-        #bx/2 + cy + e/2 = 0
-        #independent system
+        # ax + by/2 + d/2 = 0
+        # bx/2 + cy + e/2 = 0
+        # independent system
         if self.determinant != 0:
             if self._equation.a == 0:
                 y = - self._equation.d / self._equation.b
@@ -75,13 +75,13 @@ class Conic:
                 y = (self._equation.b * self._equation.d - 2 * self._equation.a * self._equation.e) / (4 * self.determinant)
                 x = - (self._equation.b * y + self._equation.d) / (2 * self._equation.a)
             self._center = self._Point(x, y)
-        #dependent system:
+        # dependent system
         elif ((self._equation.a != 0 and
                math.isclose(self._equation.e, (self._equation.b * self._equation.d) / (2 * self._equation.a))) or
               (self._equation.c != 0 and
                math.isclose(self._equation.d, (self._equation.b * self._equation.e) / (2 * self._equation.c)))):
             self._center = math.inf
-        #inconsistent system
+        # inconsistent system
         else:
             self._center = None
 
@@ -92,8 +92,8 @@ class Conic:
             self._equation.e = 0
         elif self._center == math.inf:
             if self._equation.a != 0:
-                y = 0
                 x = - self._equation.d / (2 * self._equation.a)
+                y = 0
             else:  # c != 0
                 x = 0
                 y = - self._equation.e / (2 * self._equation.c)
@@ -102,9 +102,9 @@ class Conic:
             self._equation.e = 0
 
     def _findRotationAngle(self):
+        # cot(2θ) = (a-c)/b
         if self._equation.b == 0:
             self._angle = 0
-        #cot(2θ) = (a-c)/b
         elif self._equation.a - self._equation.c == 0:
             self._angle = math.pi / 4
         else:
@@ -113,14 +113,14 @@ class Conic:
     def _rotate(self):
         if self._equation.b != 0:
             self._findRotationAngle()
-            #d' = dcos(θ) + esin(θ)
-            #e' = -dsin(θ) + ecos(θ)
+            # d' = dcos(θ) + esin(θ)
+            # e' = -dsin(θ) + ecos(θ)
             d = self._equation.d
             e = self._equation.e
             self._equation.d = d * math.cos(self._angle) + e * math.sin(self._angle)
             self._equation.e = - d * math.sin(self._angle) + e * math.cos(self._angle)
-            #a' + c' = a + c
-            #a' - c' = b√1+((a-c)/b)²
+            # a' + c' = a + c
+            # a' - c' = b√1+((a-c)/b)²
             a = self._equation.a
             c = self._equation.c
             self._equation.a = (a + c + self._equation.b * math.sqrt(1 + ((a - c) / self._equation.b) ** 2)) / 2
@@ -128,10 +128,10 @@ class Conic:
             self._equation.b = 0
 
     def _findName(self):
-        #nothing, point, circle, ellipse, hyperbola or intersecting lines
+        # nothing, point, circle, ellipse, hyperbola or intersecting lines
         if isinstance(self._center, self._Point):
             if self._equation.f == 0:
-                #ax² + cy² = 0
+                # ax² + cy² = 0
                 if self._equation.a * self._equation.c < 0:
                     self._name = 'intersecting lines'
                 else:
@@ -139,7 +139,7 @@ class Conic:
             else:
                 A = - self._equation.a / self._equation.f
                 C = - self._equation.c / self._equation.f
-                #Ax² + Cy² = 1
+                # Ax² + Cy² = 1
                 if A * C < 0:
                     self._name = 'hyperbola'
                 elif A < 0 and C < 0:
@@ -149,16 +149,16 @@ class Conic:
                         self._name = 'circle'
                     else:
                         self._name = 'ellipse'
-        #nothing, parallel lines or coincident lines
+        # nothing, parallel lines or coincident lines
         elif self._center == math.inf:
-            #ax² + f = 0 or cy² + f = 0
+            # ax² + f = 0 or cy² + f = 0
             if self._equation.f == 0:
                 self._name = 'coincident lines'
             elif self._equation.a * self._equation.f < 0 or self._equation.c * self._equation.f < 0:
                 self._name = 'parallel lines'
             else:
                 self._name = 'nothing'
-        #nothing or parabola
+        # nothing or parabola
         elif self._center == None:
             if self._equation.a != 0 and self._equation.e != 0:
                 self._name = 'parabola'
